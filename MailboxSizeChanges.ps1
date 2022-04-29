f5129407ac14847be12b97755a8f7f481ea2050e
ChrisMitchhell <104199932+ChrisMitchhell@users.noreply.github.com>
2022-04-29 09:43:02 +0100

    Add files via upload

---
+++ MailboxSizeChanges.ps1
@@ -0,0 +1,10 @@
+﻿
+#Single Mailbox
+Set-Mailbox Gordon.Coyle@escaperecruitment.com -ProhibitSendQuota 50GB -ProhibitSendReceiveQuota 50GB -IssueWarningQuota 49GB
+
+#All mailboxes
+Get-Mailbox | Set-Mailbox -ProhibitSendQuota < Value > -ProhibitSendReceiveQuota < Value > -IssueWarningQuota < Value >
+
+#View all mailboxs
+Get-Mailbox | fl DisplayName,IssueWarningQuota,ProhibitSendQuota,ProhibitSendReceiveQuota,UseDatabaseQuotaDefaults
+
